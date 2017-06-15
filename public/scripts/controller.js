48838d7c101e7a41faa175c032742c5186c5cd53
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-06-15 16:07:50 -0500

    finished project

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -1,13 +1,28 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MainController', function(GetGifs) {
-  console.log('angular loadeddd');
   var vm = this;
   vm.searchField = '';
+  vm.imageArray = [];
+
+  //get random image
   vm.getRandom = function() {
-    console.log(GetGifs.getRandom());
     GetGifs.getRandom().then(function() {
-      vm.imageUrl = GetGifs.imageUrl;
+      vm.imageArray = [];
+      vm.imageArray.push(GetGifs.imageUrl);
     });
-  };
+  }; //end get random image function
+
+  //search gifs
+  vm.searchGifs = function() {
+    console.log('in search');
+    GetGifs.searchGifs(vm.searchField).then(function() {
+      vm.imageArray = [];
+      for (var i = 0; i < GetGifs.imageArray.length; i++) {
+        console.log(GetGifs.imageArray);
+        vm.imageArray.push(GetGifs.imageArray[i].images.downsized_large.url);
+      }
+    });
+  }; //end search gifs
+
 });
--- public/scripts/service.js
+++ public/scripts/service.js
@@ -1,7 +1,7 @@
 myApp.service('GetGifs', function($http) {
   var gg = this;
 
-  this.getRandom = function() {
+  gg.getRandom = function() {
     console.log('getting random');
     var url = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC';
     return $http.get(url).then(function(response) {
@@ -9,6 +9,13 @@ myApp.service('GetGifs', function($http) {
       gg.imageUrl = response.data.data.image_url;
     });
   }; //end get random
-
+  gg.searchGifs = function(searchTerm) {
+    console.log('searching gifs', searchTerm);
+    var url = 'http://api.giphy.com/v1/gifs/search?q=' + searchTerm + '&api_key=dc6zaTOxFJmzC';
+    return $http.get(url).then(function(response) {
+      console.log(response);
+      gg.imageArray = response.data.data;
+    });
+  };
 
 });
