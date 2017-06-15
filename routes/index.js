154ccef9c95d1535429f30e1ff9b129e78f23a1d
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-06-15 11:46:47 -0500

    cleared the repo to start from scratch

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-
-router.get('/', function(req, res) {
-  res.sendFile(path.resolve('public/views/index.html'));
-});
-
-module.exports = router;
